86c9cf30dcdb2927554f359310188240f79e81d3
fuchunhui <fuchuhu7@gmail.com>
2022-08-04 14:14:39 +0800

    feat: add tailwincss

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,8 @@
+/** @type {import('prettier').Config} */
 module.exports = {
   singleQuote: true,
   endOfLine: 'lf',
   tabWidth: 2,
   trailingComma: 'es5',
+  plugins: [require('prettier-plugin-tailwindcss')],
 };
