51bcb7f772b75e708bc8524159922a8e53842c4d
fuchunhui <fuchuhu7@gmail.com>
2022-08-04 12:41:37 +0800

    feat: add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,21 @@
 module.exports = {
-    env: {
-      browser: true,
-      node: true,
-      es2022: true
-    },
-    extends: [
-        "eslint:recommended",
-        "@nuxtjs/eslint-config-typescript",
-        'plugin:nuxt/recommended',
-        "plugin:vue/vue3-essential",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    parserOptions: {
-        ecmaVersion: "latest",
-        parser: "@typescript-eslint/parser",
-        sourceType: "module"
-    },
-    plugins: [
-        "vue",
-        "@typescript-eslint"
-    ]
-}
+  env: {
+    browser: true,
+    node: true,
+    es2022: true,
+  },
+  extends: [
+    'eslint:recommended',
+    '@nuxtjs/eslint-config-typescript',
+    'plugin:nuxt/recommended',
+    'plugin:vue/vue3-essential',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  plugins: ['vue', '@typescript-eslint'],
+};
