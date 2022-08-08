f9cb9db9a3331299c09059f59e67fd48b62e3590
fuchunhui <fuchuhu7@gmail.com>
2022-08-08 19:45:13 +0800

    feat: router

--- app.vue
+++ app.vue
@@ -1,3 +1,20 @@
 <template>
-  <div>123</div>
+  <div class="hidden">
+    <!-- These two links are for generate.crawler -->
+    <nuxt-link to="zh/get-started">中文</nuxt-link>
+    <nuxt-link to="en/get-started">English</nuxt-link>
+  </div>
 </template>
+
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+const router = useRouter();
+// import Vue from 'vue';
+
+onMounted(() => {
+  router.push('get-started');
+  console.log('mount!!!');
+});
+</script>
+
+<style></style>
---
+++ contents/en/get-started.md
@@ -0,0 +1,7 @@
+# Get Started
+
+## Getting Less
+
+## Including Less
+
+And this is your first chart with Less!
